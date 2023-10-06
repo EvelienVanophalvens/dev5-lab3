e5ab7c1e0cebc046f9657cb12fbd4572eafdd3fb
Evelien Vanophalvens <Evelien.Vanophalvens@telenet.be>
2023-10-06 10:55:20 +0200

    Feat: get location

---
+++ app.js
@@ -0,0 +1,29 @@
+class App{
+    constructor(){
+        this.getLocation();
+        this.lat;
+        this.lng;
+    }
+
+    getLocation(){
+        navigator.geolocation.getCurrentPosition(this.gotLocation.bind(this), this.errorLocation.bind(this));
+    }
+
+    gotLocation(result){
+        this.lat = result.coords.latitude;
+        this.lng = result.coords.longitude;
+        this.getWeather();
+    }
+
+    errorLocation(err){
+        console.log(err);
+    }
+
+  
+
+
+
+
+}
+
+let app = new App();
